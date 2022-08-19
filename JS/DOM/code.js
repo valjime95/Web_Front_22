115a801a4a27226ab3dcb2da2a16fc2fae6df42b
Valeria-Jimeno <valjime95@gmail.com>
2022-08-19 17:25:02 -0500

    reloj

---
+++ JS/DOM/code.js
@@ -0,0 +1,14 @@
+// MANIPULACION DEL DOM
+
+// Cambiar el estilo de un elemento
+const title = document.getElementsByClassName("boldMont");
+// title.style.color= 'red';
+
+// console.log(title[0]);
+
+// for (var i = 0; i<=title.length; i++) {
+// 	title[i].style.color='red';
+// }
+
+
+// Creando Elementos
---
+++ JS/DOM/dom.js
@@ -0,0 +1,26 @@
+// MANIPULACION DEL DOM
+
+
+// getElementById()
+const list = document.getElementById("lista");
+// console.log(list);
+
+
+// getElementsByClassName()
+const clase = document.getElementsByClassName("section2");
+// console.log(clase);
+
+
+// getElementsByTagName()
+const seccion = document.getElementsByTagName("section");
+// console.log(seccion[0]);
+
+
+// querySelector()
+const articulo = document.querySelector("article");
+// console.log(articulo);
+
+
+// querySelectorAll()
+const articulos = document.querySelectorAll("article");
+// console.log(articulos);
